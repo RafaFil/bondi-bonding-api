db7dd73dd3db0427857f387df22bca146ec478aa
RafaFil <filardisafa@gmail.com>
2022-11-19 01:43:31 -0300

    conexion db no anda

--- src/services/static.service.js
+++ src/services/static.service.js
@@ -1,12 +1,14 @@
-const dbo = require('../configs/db.config');
-const dbConnect = dbo.getDb();
+/*const dbo = require('../configs/db.config');
+const dbConnect = await dbo.getDb();*/
+const { getDb } = require('../configs/db.config');
+const dbConnect = await getDb();
 
 async function getByType(type){
-    return (await dbConnect)
-    .collection(process.env.STATIC)
+    return await dbConnect
+    .collection(process.env.STATIC_COLLECTION)
     .aggregate([
-        {$match : { type : `${type}`}},
-        {$project: {"_id": 0,"type":0}}
+        { $match : { type : type } },
+        { $project: { _id: 0, type: 0 } }
     ])
     .toArray();
 }
