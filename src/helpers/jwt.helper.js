170a3114b5e1d6fdb18b8cb7b859069ff91cda19
Walter <Tano.walter@hotmail.com>
2022-11-20 18:05:13 -0300

    started working on auth

--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken';
 
 const EXPIRES_IN = process.env.JWT_EXPIRES_IN;
-const SEED = process.env.SECRET_JWT_SEED;
+const SECRET_JWT_SEED = process.env.SECRET_JWT_SEED;
 
-export const generateJWT = ( uid, name ) => {
+const generateJWT = ( uid, name ) => {
     const payload = { uid, name };
 
     return new Promise( (resolve, reject) => {
-        jwt.sign( payload, process.env.SECRET_JWT_SEED, {
-            expiresIn: '24h'
+        jwt.sign( payload, SECRET_JWT_SEED, {
+            expiresIn: EXPIRES_IN
         }, (err, token) => {
             if ( err ) {
                 console.log(err);
@@ -19,3 +19,7 @@ export const generateJWT = ( uid, name ) => {
         });
     });
 };
+
+module.exports = {
+    generateJWT
+}
---
+++ src/middlewares/validateJWT.middleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+const validateJWT = ( req, res = response, next) => {
+    let token = req.header('Authorization');
+
+    if ( !token || !token.startsWith('Bearer ') ) {
+        return res.status(400).json({
+            success: false,
+            message: 'Wrong authenticacion scheme. Please use the Bearer scheme.'
+        });
+    }
+
+    // Removing the "Bearer " part from the token
+    token = token.replace(/^Bearer\s+/, '');
+
+    try {
+        const { uid, name } = jwt.verify( token, process.env.SECRET_JWT_SEED );
+        req.uid = uid;
+        req.name = name;
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token invalid or expired.'
+        });
+    }
+
+    next();
+}
